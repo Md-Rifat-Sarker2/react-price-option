2cec76653c8c372d2d53cb4c172d412305ae4452
Md-Rifat-Sarker2 <mdrifatsarker.edu@gmail.com>
2025-03-15 11:45:38 +0600

    featch data using axios and cearte a bar chart

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import LineChart from './Components/LineChart/LineChart'
 import NavBar from './Components/NavBar/NavBar'
+import Phones from './Components/Phones/Phones'
 import PriceOptions from './Components/PriceOptions/PriceOptions'
 // import DaisyNav from './Components/DaisyNav/DaisyNav'
 
@@ -13,6 +14,7 @@ function App() {
       <h1 className='text-7xl bg-rose-600'>Vite + React</h1>
       <PriceOptions></PriceOptions>
       <LineChart></LineChart>
+      <Phones></Phones>
     </>
   )
 }
